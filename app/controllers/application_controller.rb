1a6d0c6cc852750a118ac9728b8c95fc88b6aa19
sirokuma <lord_ring_2006@icloud.com>
2020-07-19 20:31:21 +0000

    hello3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-     render html:"hello world everyone2"
+     render html:"hello world everyone3"
   end
   
 end
