e45cf479b3e01e0ec5f9d2ce7cabb38fbeedd5cc
sirokuma <lord_ring_2006@icloud.com>
2020-07-20 18:25:37 +0000

    first

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'static_pages/home'
 
-    root 'application#hello'
+  get 'static_pages/help'
+
+  root 'application#hello'
 
 end
