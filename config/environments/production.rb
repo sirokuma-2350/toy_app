e664337d72c1777f211547264abf4d1cbdb616cc
sirokuma <lord_ring_2006@icloud.com>
2020-08-07 14:41:30 +0000

    modyfy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://stormy-beyond-34718s.herokuapp.com'
+  host = 'https://serene-springs-91718.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
