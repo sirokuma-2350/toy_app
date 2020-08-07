65aeb643b1b52f1a6043d4fa20e5e1c8661aa243
sirokuma <lord_ring_2006@icloud.com>
2020-08-07 13:17:15 +0000

    add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'arcane-caverns-76528.herokuapp.com'
+  host = 'https://arcane-caverns-76528.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
