6c26922a889f19ff6177e9490007cdeba96ef642
sirokuma <lord_ring_2006@icloud.com>
2020-08-19 01:53:31 +0000

    Add user microposts

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,11 +2,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
   
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
   
   storage :file
 
